d12b3d513d6b93bf0320a3d0e422ccc33f9a30a6
Tanguy Escaron <tangue@amazon.com>
2021-06-12 11:52:15 +0100

    ca va fin du merge

--- joined-together/Environment/Diams/Diam.gd
+++ joined-together/Environment/Diams/Diam.gd
@@ -4,6 +4,14 @@ export (float) var rotation_speed = 2
 
 export (int) var value = 2
 export (int) var category = 1
+export (PackedScene) var Particule
 
 func _process(delta):
 	rotate(rotation_speed * delta)
+
+func _exit_tree():
+	var explosion = null
+	explosion = Particule.instance()
+	explosion.position = self.position
+	get_tree().current_scene.add_child(explosion)
+	print("lol", explosion.position.x, self.position.x)
