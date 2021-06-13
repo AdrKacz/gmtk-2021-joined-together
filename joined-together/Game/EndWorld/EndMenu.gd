6ed75b7791d022ebf32f5b058e663f84072f7734
AdrKacz <adrien.kaczmarek@gmail.com>
2021-06-13 18:34:17 +0100

    Add UI END

--- joined-together/Game/EndWorld/EndMenu.gd
+++ joined-together/Game/EndWorld/EndMenu.gd
@@ -7,8 +7,12 @@ func _ready():
 	var time = Session.time
 	var minutes = floor(time / 60)
 	var seconds = time % 60
-	$CenterContainer/VBoxContainer/Time/Time.text = "%02d:%02d" % [minutes, seconds]
-	$CenterContainer/VBoxContainer/Score/Score.text = str(Session.value)
+	$CenterContainer/VBoxContainer2/HBoxContainer/Values/Time.text = "%02d:%02d" % [minutes, seconds]
+	
+	var categories_count = Session.categories_count
+	$CenterContainer/VBoxContainer2/HBoxContainer/Values/Yellow.text = str(categories_count[0])
+	$CenterContainer/VBoxContainer2/HBoxContainer/Values/Green.text = str(categories_count[1])
+	$CenterContainer/VBoxContainer2/HBoxContainer/Values/Red.text = str(categories_count[2])
 	
 func _on_Play_pressed():
 	Session.restart_game()
